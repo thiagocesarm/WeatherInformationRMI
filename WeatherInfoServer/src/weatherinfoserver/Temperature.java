06c467dade650a0f7034c7ad3edafdaa49d75648
Carlos Vieira <vieiramecarlos@gmail.com>
2018-08-29 20:17:05 -0300

    Add temperature class

---
+++ WeatherInfoServer/src/weatherinfoserver/Temperature.java
@@ -0,0 +1,42 @@
+package weatherinfoserver;
+
+import java.io.Serializable;
+
+public class Temperature implements Serializable {
+	Double kelvin;
+	
+	Temperature() {
+		this.kelvin = null;
+	}
+	
+	Temperature(double k) {
+		if (k < 0) {
+			throw new IllegalArgumentException(
+					"Kelvin temperatures not valid below zero");
+		}
+		
+		this.kelvin = k;
+	}
+	
+	double getCelsius() {
+		return Temperature.kelvinToCelsius(this.kelvin);
+	}
+	
+	double getKelvin() {
+		return this.kelvin;
+	}
+	
+	double getFahrenheit() {
+		return Temperature.kelvinToFahrenheit(this.kelvin);
+	}
+	
+	
+	static double kelvinToCelsius(double k) {
+		return k - 273.15;
+	}
+	
+	static double kelvinToFahrenheit(double k) {
+		return k * 9d/5d - 459.67d;
+	}
+
+}
